1ed92b2a771a3d4fdcdb78f1b8198ce82369077d
Raziel619 <raziel619dev@gmail.com>
2023-06-02 09:25:03 -0400

    Update Contact

--- src/App.jsx
+++ src/App.jsx
@@ -22,14 +22,15 @@ const App = () => {
           <Navbar></Navbar>
           <Hero></Hero>
         </div>
-        <About />
-        <Experience />
-        <Frameworks />
-        <Toolbox />
-        <Languages />
-        <Works />
-        <Externals />
+
         <div className="relative z-0">
+          <About />
+          <Experience />
+          <Frameworks />
+          <Toolbox />
+          <Languages />
+          <Works />
+          <Externals />
           <Contact />
           <StarsCanvas />
         </div>
--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -19,7 +19,7 @@ const Stars = (props) => {
         <PointMaterial
           transparent
           color="#f272c8"
-          size={0.002}
+          size={0.001}
           sizeAttenuation={true}
           depthWrite={false}
         ></PointMaterial>
